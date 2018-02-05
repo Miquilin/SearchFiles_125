36ff4be8ae84b2aa86cd26093088cee6ffe8f8c4
Saex05 <33133651+Saex05@users.noreply.github.com>
2018-02-04 22:40:23 -0400

    Update the unit tests
    Edit from github editor

--- src/com/jalasoft/search_files/utils/Search_Validation_Test.py
+++ src/com/jalasoft/search_files/utils/Search_Validation_Test.py
@@ -1,24 +1,98 @@
+import unittest
 from src.com.jalasoft.search_files.utils.Search_Validation import SearchValidation
 
-print("**********Primer Test************************")
-string_1 = ["HOLA", "hola", "1234,", "la mu"]
-string_2 = "HOLA mundo"
-flag = 1
-message = "{string_1} existe en {string_2}: {boolean}"
-for cadena in string_1:
-    ValueBoolean = SearchValidation.the_string1_exists_within_the_string2(SearchValidation, cadena, string_2, flag)
-    print(message.format(string_1=cadena, string_2=string_2, boolean=ValueBoolean))
+
+class SearchValidationTest(unittest.TestCase):
+    def test_string1_exists_within_string2_method_return_true_if_string_1_exist_within_string2_with_case_sensitive_disable(self):
+        search_validation = SearchValidation()
+        string_1 = "LA mu"
+        string_2 = "HOLA mundo"
+        case_sensitive = False
+        value_boolean = SearchValidation.string1_exists_within_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertTrue(value_boolean)
+
+    def test_string1_exists_within_string2_method_return_false_if_string_1_does_not_exist_within_string2_with_case_sensitive_disable(self):
+        search_validation = SearchValidation()
+        string_1 = "la MU"
+        string_2 = "HOLA mundo"
+        case_sensitive = False
+        value_boolean = SearchValidation.string1_exists_within_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertFalse(value_boolean)
+
+    def test_string1_exists_within_string2_method_return_true_if_string_1_exist_within_string2_with_case_sensitive_enable(self):
+        search_validation = SearchValidation()
+        string_1 = "LA mu"
+        string_2 = "HOLA mundo"
+        case_sensitive = True
+        value_boolean = SearchValidation.string1_exists_within_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertTrue(value_boolean)
+
+    def test_string1_exists_within_string2_method_return_true_if_string_1_exists_as_uppercase_or_lowercase_within_string2_with_case_sensitive_enable(self):
+        search_validation = SearchValidation()
+        string_1 = "la MU"
+        string_2 = "HOLA mundo"
+        case_sensitive = True
+        value_boolean = SearchValidation.string1_exists_within_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertTrue(value_boolean)
+
+    def test_string1_equal_to_string2_method_return_true_if_string_1_is_equal_to_string2_with_case_sensitive_disable(self):
+        search_validation = SearchValidation()
+        string_1 = "HOLA mundo"
+        string_2 = "HOLA mundo"
+        case_sensitive = False
+        value_boolean = SearchValidation.string1_equal_to_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertTrue(value_boolean)
+
+    def test_string1_equal_to_string2_method_return_false_if_string_1_is_not_equal_to_string2_with_case_sensitive_disable(self):
+        search_validation = SearchValidation()
+        string_1 = "hola MUNDO"
+        string_2 = "HOLA mundo"
+        case_sensitive = False
+        value_boolean = SearchValidation.string1_equal_to_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertFalse(value_boolean)
+
+    def test_string1_equal_to_string2_method_return_true_if_string_1_is_equal_to_string2_with_case_sensitive_enable(self):
+        search_validation = SearchValidation()
+        string_1 = "HOLA mundo"
+        string_2 = "HOLA mundo"
+        case_sensitive = True
+        value_boolean = SearchValidation.string1_equal_to_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertTrue(value_boolean)
+
+    def test_string1_equal_to_string2_method_return_true_if_string_1_with_upper_and_lower_case_is_equal_to_string2_with_case_sensitive_enable(self):
+        search_validation = SearchValidation()
+        string_1 = "hola MUNDO"
+        string_2 = "HOLA mundo"
+        case_sensitive = True
+        value_boolean = SearchValidation.string1_equal_to_string2(
+            search_validation, string_1, string_2, case_sensitive)
+        self.assertTrue(value_boolean)
+
+    def test_is_less_than_or_equal_method_return_true_if_the_bit_size_is_less_than_to_mega_bit_size(self):
+        num_bit = 1000000
+        num_mega_bit = 2
+        value_boolean = SearchValidation().is_less_than_or_equal(num_bit, num_mega_bit)
+        self.assertTrue(value_boolean)
+
+    def test_is_less_than_or_equal_method_return_true_if_the_bit_size_is_equals_to_mega_bit_size(self):
+        num_bit = 2000000
+        num_mega_bit = 2
+        value_boolean = SearchValidation().is_less_than_or_equal(num_bit, num_mega_bit)
+        self.assertTrue(value_boolean)
+
+    def test_is_less_than_or_equal_method_return_false_if_the_bit_size_is_bigger_than_to_mega_bit_size(self):
+        num_bit = 3000000
+        num_mega_bit = 2
+        value_boolean = SearchValidation().is_less_than_or_equal(num_bit, num_mega_bit)
+        self.assertFalse(value_boolean)
 
 
-print("**********Segundo Test************************")
-string_1 = ["HOLA mundo", "hola mundo", "HOLAmundo", "123456", " "]
-string_2 = "HOLA mundo"
-flag = 1
-message = "{string_1} es igual a {string_2}: {boolean}"
-for cadena in string_1:
-    ValueBoolean = SearchValidation.string1_equal_to_string2(SearchValidation, cadena, string_2, flag)
-    print(message.format(string_1=cadena, string_2=string_2, boolean=ValueBoolean))
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
