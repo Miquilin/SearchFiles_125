19ca3d7118cd7191f8bb0e8027dd9faa3dfd30e1
Saex05 <33133651+Saex05@users.noreply.github.com>
2018-02-07 00:50:57 -0400

    Fixed observations

--- src/com/jalasoft/search_files/utils/Search_Validation.py
+++ src/com/jalasoft/search_files/utils/Search_Validation.py
@@ -9,28 +9,28 @@ class SearchValidation(object):
     def string1_exists_within_string2(self, string_1, string_2, flag):
         case_sensitive = bool(flag)
         if case_sensitive is True:
-            search_match = re.search(string_1, string_2, re.IGNORECASE)
-            if search_match is None:
-                return False
-            else:
-                return True
-        else:
             search_match = re.search(string_1, string_2)
             if search_match is None:
                 return False
             else:
                 return True
+        else:
+            search_match = re.search(string_1, string_2, re.IGNORECASE)
+            if search_match is None:
+                return False
+            else:
+                return True
 
-    def string1_equal_to_string2(self, string_1, string_2, flag):
+    def is_string1_equal_to_string2(self, string_1, string_2, flag):
         case_sensitive = bool(flag)
         if case_sensitive is True:
-            search_match = re.match(string_1, string_2, re.IGNORECASE)
+            search_match = re.match(string_1, string_2)
             if search_match is None:
                 return False
             else:
                 return True
         else:
-            search_match = re.match(string_1, string_2)
+            search_match = re.match(string_1, string_2, re.IGNORECASE)
             if search_match is None:
                 return False
             else:
@@ -38,7 +38,7 @@ class SearchValidation(object):
 
     def is_less_than_or_equal(self, bit_size, mega_bit_size):
         num_bit = bitmath.Bit(bit_size)
-        num_mega_bit = bitmath.Mb(mega_bit_size)
+        num_mega_bit = bitmath.Mib(mega_bit_size)
         if num_bit <= num_mega_bit:
             return True
         else:
@@ -47,7 +47,7 @@ class SearchValidation(object):
     def is_bigger_than_other(self, bit_size, mega_bit_size):
         # it is possible that the is_bigger_than_other() method is redundant
         num_bit = bitmath.Bit(bit_size)
-        num_mega_bit = bitmath.Mb(mega_bit_size)
+        num_mega_bit = bitmath.Mib(mega_bit_size)
         if num_bit < num_mega_bit:
             return True
         else:
