fcc64cfab356e778eec51f257c68da91d5a575aa
ALEX\Saex <saex050988@gmail.com>
2018-02-10 23:46:26 -0400

    Add loggers in all created validations change in the formater of the logger:  time [path][type level] module::method. - Message Create as comment the CONFIG_PATH_FILE to see the args in the config/loging.conf

--- definition.py
+++ definition.py
@@ -1,4 +1,8 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.join(ROOT_DIR, 'config/logging.conf')
+#Linux: Change the \ to / (config/logging.conf)
+CONFIG_PATH = os.path.join(ROOT_DIR, 'config\logging.conf')
+
+#CONFIG_PATH_SEARCH_FILE = os.path.join(ROOT_DIR, 'log\search_files.log')
+#print(CONFIG_PATH_SEARCH_FILE)
