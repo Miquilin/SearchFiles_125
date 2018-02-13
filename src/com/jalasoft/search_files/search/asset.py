769314e7b958cb0bc556a24a4989eefb1f675e87
ovandononi <ovando.noni@gmail.com>
2018-02-13 14:16:48 -0400

    Search by date: Menu shoud send a Date type as value It is a new search advance. - Search_criteria need to be set with a Date value on following fields: -- set_less_creation_date(Date) and/or set_bigger_creation_date(Date) -- set_less_modification_date(Date) and/or set_bigger_modification_date(Date)

--- src/com/jalasoft/search_files/search/asset.py
+++ src/com/jalasoft/search_files/search/asset.py
@@ -1,5 +1,5 @@
 import os
-
+import datetime
 
 class Asset(object):
 
@@ -7,6 +7,28 @@ class Asset(object):
         self._path = path
         self._is_file = is_file
         self._size = os.path.getsize(path)
+        attributes = os.stat(path)
+        self._st_access_time = datetime.datetime.fromtimestamp(attributes[7])
+        self._st_modification_time = datetime.datetime.fromtimestamp(attributes[8])
+        self._st_creation_time = datetime.datetime.fromtimestamp(attributes[9])
+        # supported only on unix
+        self._st_uid_user_id = attributes[4]
+        self._st_gid_group_id = attributes[5]
+
+    def get_st_access_time(self):
+        return self._st_access_time
+
+    def get_st_modification_time(self):
+        return self._st_modification_time
+
+    def get_st_creation_time(self):
+        return self._st_creation_time
+
+    def get_st_uid_user_id(self):
+        return self._st_uid_user_id
+
+    def get_st_gid_group_id(self):
+        return self._st_gid_group_id
 
     def get_path(self):
         return self._path
