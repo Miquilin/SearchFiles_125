498591916d5819f0ca5da61a7d0632f4f61c60b8
ALEX\Saex <saex050988@gmail.com>
2018-02-18 20:18:29 -0400

    Add Documentation Comment Blocks

--- src/com/jalasoft/search_files/utils/menu_validation.py
+++ src/com/jalasoft/search_files/utils/menu_validation.py
@@ -5,10 +5,19 @@ from src.com.jalasoft.search_files.utils.logging_config import logger
 
 
 class Validation(object):
+    """
+    Validations required by Menu
+    """
     def __init__(self):
         pass
 
     def is_number(self, string_value):
+        """
+        This method returns True when the string_value is a positive number otherwise return False
+
+        :param string_value:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         boolean_contain = string_value.isdigit()
         logger.debug("The value returned is: %s", boolean_contain)
@@ -16,6 +25,15 @@ class Validation(object):
         return boolean_contain
 
     def is_the_range(self, number, upper_limit, lower_limit):
+        """
+        This method returns True when the number is between upper_limit number and lower_limit number
+        otherwise return False
+
+        :param number:
+        :param upper_limit:
+        :param lower_limit:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         number = int(number)
         if number >= lower_limit and number <= upper_limit:
@@ -27,6 +45,12 @@ class Validation(object):
         return boolean_contain
 
     def attempt_maximum(self, attempts):
+        """
+        This method returns True if the attempts number is less than or equal to 5 otherwise return False
+
+        :param attempts:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         attempts = int(attempts)
         if attempts <= 5:
@@ -38,6 +62,12 @@ class Validation(object):
         return boolean_contain
 
     def is_path_valid(self, directory):
+        """
+        This method returns True when the string is a path valid otherwise return False
+
+        :param directory: (a path)
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         directory = str(directory)
         boolean_contain = os.path.exists(directory)
@@ -46,6 +76,11 @@ class Validation(object):
         return boolean_contain
 
     def has_valid_characters(self, string_value):
+        """
+        This method returns True when the string value contains alphanumeric characters otherwise return False
+        :param string_value:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         string_value = str(string_value)
         boolean_contain = string_value.isalnum()
@@ -54,6 +89,11 @@ class Validation(object):
         return boolean_contain
 
     def is_blank(self, string_value):
+        """
+        This method returns True if the string_value is blank otherwise return False
+        :param string_value:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         string_value = str(string_value)
         if string_value and string_value.strip():
@@ -65,6 +105,13 @@ class Validation(object):
         return boolean_contain
 
     def is_valid_date(self, str_date):
+        """
+        This method returns True if the str_date contain the following formats %Y-%m-%d %H:%S:%M and y-%m-%d %H:%S:%M
+        otherwise return False
+
+        :param str_date:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         regex = re.compile("(\d{1,4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})")
         match = regex.match(str_date)
@@ -88,6 +135,12 @@ class Validation(object):
         return boolean_contain
 
     def has_valid_characters_adv(self, string_value):
+        """
+        this method returns False if string_value contains the following characters |<>"?:\/ otherwise return True
+
+        :param string_value:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         string_value = str(string_value)
         if '|' in string_value or '<' in string_value or '>' in string_value or '"' in string_value or '?' in string_value or ':' in string_value or '/' in string_value or '\\' in string_value:
--- src/com/jalasoft/search_files/utils/search_validation.py
+++ src/com/jalasoft/search_files/utils/search_validation.py
@@ -5,10 +5,22 @@ from src.com.jalasoft.search_files.utils.logging_config import logger
 
 
 class SearchValidation(object):
+    """
+    Validations required by Search
+    """
     def __init__(self):
         pass
 
     def string1_exists_within_string2(self, string_1, string_2, flag):
+        """
+        This method determines whether the string_1 exists within the string_2, besides doing this search with and
+        without sensitive case(flag= True or False)
+
+        :param string_1:
+        :param string_2:
+        :param flag:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         case_sensitive = bool(flag)
         if case_sensitive is True:
@@ -28,6 +40,15 @@ class SearchValidation(object):
         return boolean_contain
 
     def is_string1_equal_to_string2(self, string_1, string_2, flag):
+        """
+        This method determines whether the string_1 is equals to the string_2, besides doing this search with and
+        without sensitive case(flag= True or False)
+
+        :param string_1:
+        :param string_2:
+        :param flag:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         case_sensitive = bool(flag)
         if case_sensitive is True:
@@ -47,6 +68,13 @@ class SearchValidation(object):
         return boolean_contain
 
     def is_less_than_or_equal(self, bit_size, mega_bit_size):
+        """
+        This method returns True if bit_size is less than or equals to mega_bit_size otherwise return False
+
+        :param bit_size:
+        :param mega_bit_size:
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         num_bit = bitmath.Bit(bit_size)
         num_mega_bit = bitmath.Mib(mega_bit_size)
@@ -59,6 +87,13 @@ class SearchValidation(object):
         return boolean_contain
 
     def is_bigger_than_other(self, bit_size, mega_bit_size):
+        """
+        This method returns True if bit_size is bigger than mega_bit_size otherwise return False
+
+        :param bit_size:
+        :param mega_bit_size:
+        :return otherwise return False:
+        """
         logger.info("Starting the method")
         num_bit = bitmath.Bit(bit_size)
         num_mega_bit = bitmath.Mib(mega_bit_size)
@@ -71,6 +106,11 @@ class SearchValidation(object):
         return boolean_contain
 
     def is_windows(self):
+        """
+        this method returns True if the OS is Windows
+
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         operation_system = sys.platform
         search_match = re.search("win", operation_system)
@@ -83,6 +123,11 @@ class SearchValidation(object):
         return boolean_contain
 
     def is_linux(self):
+        """
+        this method returns True if the OS is Linux
+
+        :return boolean_contain:
+        """
         logger.info("Starting the method")
         operation_system = sys.platform
         search_match = re.search("linux", operation_system)
